7c5fe55a9bbe4b6839e221f8210131271662bb41
Vlad Gritsenko <vladxmas@gmail.com>
2017-05-14 15:51:59 +0300

    MoCart finished

---
+++ app/js/main.js
@@ -0,0 +1,92 @@
+var hours = document.querySelector('.hours'),
+    minutes = document.querySelector('.minutes'),
+    seconds = document.querySelector('.seconds');
+
+
+setInterval(function() {
+  if (seconds.innerHTML <= 0) {
+    seconds.innerHTML = 60;
+    minutes.innerHTML -= 1;
+  }
+
+  if (minutes.innerHTML <=0) {
+    minutes.innerHTML = 59;
+    hours.innerHTML -= 1;
+  }
+  seconds.innerHTML -= 1;
+
+  seconds.innerHTML = ('0' + seconds.innerHTML).slice(-2);
+  minutes.innerHTML = ('0' + minutes.innerHTML).slice(-2);
+  hours.innerHTML = ('0' + hours.innerHTML).slice(-2);
+
+}, 1000);
+
+//slick carousel
+
+$(document).ready(function(){
+  $('.discounts').slick({
+    dots: false,
+    infinite: false,
+    variableWidth: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.products').slick({
+    dots: false,
+    infinite: true,
+    variableWidth: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //       arrows: false
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+    // ]
+  });
+});
