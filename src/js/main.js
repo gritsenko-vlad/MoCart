b91eec18965d146cdae65f48946d509c03540123
Your Name <you@example.com>
2017-10-26 18:06:24 +0300

    minor refactoring, change gulp boilerplate

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,18 @@
-var hours = document.querySelector('.hours'),
-    minutes = document.querySelector('.minutes'),
-    seconds = document.querySelector('.seconds');
-
-
-setInterval(function() {
-  if (seconds.innerHTML <= 0) {
-    seconds.innerHTML = 60;
-    minutes.innerHTML -= 1;
-  }
-
-  if (minutes.innerHTML <=0) {
-    minutes.innerHTML = 59;
-    hours.innerHTML -= 1;
-  }
-  seconds.innerHTML -= 1;
-
-  seconds.innerHTML = ('0' + seconds.innerHTML).slice(-2);
-  minutes.innerHTML = ('0' + minutes.innerHTML).slice(-2);
-  hours.innerHTML = ('0' + hours.innerHTML).slice(-2);
-
-}, 1000);
-
-
-
-
 $(document).ready(function(){
 
-  //Slick carousels
   $('.discounts').slick({
     dots: false,
-    infinite: false,
+    infinite: true,
     variableWidth: true,
     speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1280,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           arrows: false
         }
       },
@@ -104,4 +77,30 @@ $(document).ready(function(){
       }
     ]
   });
+
+// Clock timer
+  setInterval(function() {
+    if ($('.seconds').html() <= 0) {
+
+      $('.seconds').html(60);
+      $('.minutes').html( $('.minutes').html() - 1 );
+    }
+
+    if ($('.minutes').html() <=0) {
+
+      $('.minutes').html(59);
+      $('.hours').html( $('.hours').html() - 1 );
+    }
+
+    $('.seconds').html( $('.seconds').html() - 1 );
+
+    $('.seconds').html( ('0' + $('.seconds').html()).slice(-2) );
+    $('.minutes').html( ('0' + $('.minutes').html()).slice(-2) );
+    $('.hours').html( ('0' +   $('.hours').html()).slice(-2) );
+
+  }, 1000);
+
+$('.hamburger').click(function(){
+  $('.top-links').toggleClass('active')
+});
 });
